c6018bf7c1d98926519d4547e7a5c700716916ae
Thomas Price <twprice@ncsu.edu>
2023-02-22 11:07:44 -0500

    Very basic server!

--- server/main.py
+++ server/main.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
+import pickle
+from sklearn.ensemble import AdaBoostClassifier
+import os
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
+
+    model139 = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        data_file = os.path.join(basedir, 'data/model-139.pkl')
+        self.model139 = pickle.load(open(data_file, "rb"))
+
     def get(self):
         return {'hello': 'world'}
+
+    def post(self):
+        json = request.get_json()
+        print(json)
+        code = json["CodeState"]
+        score = self.model139.predict_proba([code])
+        return [
+            {
+                "action": "ShowDiv",
+                "data": {
+                    "html": f"Score: {score}"
+                }
+            }
+        ]
 
 api.add_resource(HelloWorld, '/')
 
